7ef3a502ef5fd6a75e1523609e23ac684c33d5b5
Alexandr <A.N.Zemchenkov@gmail.com>
2023-01-25 00:39:05 +0300

    Сreated the resulting array

--- FinalTestWork/Program.cs
+++ FinalTestWork/Program.cs
@@ -1,4 +1,20 @@
 ﻿string[] array = new string[] { "Hello", "World", "My", "Name", "Is", "Alexandr" };
 Console.WriteLine("Исходный массив:");
 foreach (string s in array)
+    Console.Write(s + " ");
 Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+        count++;
+
+string[] resultArray = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3) 
+        resultArray[j++] = array[i]; 
+Console.WriteLine("\n\nРезультирующий массив:");    
+foreach (string s in resultArray)
+    Console.Write(s + " ");
+
